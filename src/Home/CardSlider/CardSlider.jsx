6f0c7446c682b0342675ca8032bb966b02f54b5f
Fadonougbo <fadougbogautier@gmail.com>
2023-05-18 12:13:12 +0100

    ajout du slider

--- src/Home/CardSlider/CardSlider.jsx
+++ src/Home/CardSlider/CardSlider.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable @babel/object-curly-spacing */
 import React from "react";
 import { useEffect } from "react";
-import Glide from '@glidejs/glide'
+import Glide from '@glidejs/glide';
 
 /**
  * Affiche un slider
@@ -10,22 +11,32 @@ import Glide from '@glidejs/glide'
 export const CardSlider=()=>{
 
     useEffect(()=>{
-        new Glide('.glide').mount()
+
+        const glide=new Glide('.glide',{
+            type:"carousel",
+            autoplay:true,
+            keyboard:true,
+            animationDuration:5000,
+            animationTimingFunc:"ease"
+        })
+
+        glide.mount()
     },[])
 
     return (
-        <div>
+        <div id="card_slider" >
             <div className="glide">
                 <div className="glide__track" data-glide-el="track">
-                <ul className="glide__slides">
-                    <li className="glide__slide">0</li>
-                    <li className="glide__slide">1</li>
-                    <li className="glide__slide">2</li>
-                </ul>
+                    <section className="glide__slides">
+                        <section className="glide__slide banner banner_1">
+                        </section>
+                        <section className="glide__slide banner banner_2">
+                        </section>
+                    </section>
                 </div>
-                <div data-glide-el="controls">
-                    <button data-glide-dir="<<">Start</button>
-                    <button data-glide-dir=">>">End</button>
+                <div className="glide__bullets" data-glide-el="controls[nav]">
+                        <button className="glide__bullet" data-glide-dir="=0"></button>
+                        <button className="glide__bullet" data-glide-dir="=1"></button>
                 </div>
             </div>
         </div>
